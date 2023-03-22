439ec52d4f32e527f9e9c7669391e4bcd705c433
Harsh <harshsharma41664@gmail.com>
2023-03-22 14:52:28 +0530

    Got the console version kind of running

---
+++ src/Project.js
@@ -0,0 +1,50 @@
+/* eslint-disable require-jsdoc */
+const ListItem = require('./listItem');
+
+// const myClass = new ListItem('lola', 'bunny', '12/12/23', 'high');
+
+class Project {
+  constructor(title) {
+    this._listItems = [];
+    this._title = title;
+    this._numOfProjects=0;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(title) {
+    this._title = title;
+  }
+
+  get lists() {
+    return this._listItems;
+  }
+
+  addToProject(listItem) {
+    this._listItems.push(listItem);
+    this._numOfProjects+=1;
+  }
+
+  removeListItem(num=(this._numOfProjects-1)) {
+    console.log(this._listItems.splice(num, 1));
+    this._numOfProjects-=1;
+  }
+}
+
+module.exports=Project;
+/*
+const myClass = new ListItem('lola', 'bunny', '12/12/23', 'high');
+const myClass2 = new ListItem('lola', 'bunny', '12/12/23', 'high');
+const myClass3 = new ListItem('lola', 'bunny', '12/12/23', 'high');
+
+const myProject = new Project('myProject');
+myProject.addToProject(myClass);
+myProject.addToProject(myClass2);
+myProject.addToProject(myClass3);
+console.log(myProject);
+myProject.removeListItem();
+console.log(myProject);
+*/
+
